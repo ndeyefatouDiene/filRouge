59075c72e850ce42f613c3f3179e94e6d73cd39e
ndeyefatouDiene <ndeyefatoudiene@outlook.com>
2020-07-27 19:43:12 +0000

    credentials

---
+++ src/Entity/Apprenant.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use App\Repository\ApprenantRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ApiResource()
+ * @ORM\Entity(repositoryClass=ApprenantRepository::class)
+ */
+class Apprenant extends User
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $statut;
+
+    /**
+     * @ORM\Column(type="boolean", nullable=true)
+     */
+    private $is_connect;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getStatut(): ?string
+    {
+        return $this->statut;
+    }
+
+    public function setStatut(?string $statut): self
+    {
+        $this->statut = $statut;
+
+        return $this;
+    }
+
+    public function getIsConnect(): ?bool
+    {
+        return $this->is_connect;
+    }
+
+    public function setIsConnect(?bool $is_connect): self
+    {
+        $this->is_connect = $is_connect;
+
+        return $this;
+    }
+}
